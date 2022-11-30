59d94e65e6c5cf12b8fadefdb43b411f838f7dec
unknown <jokuman7121@gmail.com>
2022-11-30 18:11:12 +0900

    1130

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class User(models.Model):
     use_in_migrations = True
-    id = models.IntegerField(primary_key=True, auto_created=True)
+    user_id = models.AutoField(primary_key=True)
     username = models.TextField()
     password = models.TextField()
     created_at = models.TextField()
